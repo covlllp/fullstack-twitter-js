65c1daf0fa4e5cee40a8e612b05b643c44174fe7
Colin VanLang <colinvanlang@gmail.com>
2015-01-23 14:51:18 -0500

    basic server set up

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var morgan = require('morgan');
+
+// init express instance and set morgan middleware
+var app = express();
+app.use(morgan('dev'));
+
+// listen for port 3000;
+app.listen(3000);
+console.log('server listenning');
+
+app.get('/', function(req, res) {
+	console.log('hello!');
+	res.end();
+});
